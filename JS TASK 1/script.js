17ff10d9dca25bbda1d50c4a089d8acd79491392
guruprasanna <guruprasannasubramani2001@gmail.com>
2026-06-18 11:04:18 -0800

    Task 1

---
+++ JS TASK 1/script.js
@@ -0,0 +1,30 @@
+var EmployeeName = " Guruprasanna"
+
+let EmployeeAge = " 25"
+
+const CompanyName = " Stackly"
+
+console.log("Employee Name :" + EmployeeName );
+console.log("Employee Age:" + EmployeeAge);
+console.log("Company Name:" + CompanyName);
+
+alert("Welcome to Employee Portal")
+
+
+var Employeetask = confirm(" Have you completed today's task?" );
+console.log(Employeetask);
+
+var username = prompt("Enter your name");
+console.log(username);
+
+document.writeln( "Welcome "+ username);
+
+console.warn("Task submitted successfully");
+
+console.error("Task submission failed");
+
+
+EmployeeAge = "27"
+
+console.log(" Employee Age:" + EmployeeAge);
+
